1ab3a98a4f3acb3e2a67d6aa125e6a70e75c7adc
mdjameel04 <jameeltony67@gmail.com>
2025-11-29 00:54:35 +0530

    added images

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
+import { SignUpButton } from '@clerk/nextjs'
 const Homepage = () => {
   return (
     <div>
     <Button> click me</Button>
-  
+  <SignUpButton> signup</SignUpButton>
     </div>
   )
 }
