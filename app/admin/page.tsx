957a1d2efc349bf57a77b24bb20ed9eecef90e4b
mdjameel04 <jameeltony67@gmail.com>
2025-11-29 23:43:32 +0530

    added adminBoard

---
+++ app/admin/page.tsx
@@ -0,0 +1,22 @@
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
+import AdminDashboardClient from "./AdminDashboardClient";
+
+
+async function AdminPage (){
+ const user = await currentUser();
+ 
+ // user is not logged in 
+if(!user) redirect("/")
+    
+    const adminEmail = process.env.ADMIN_EMAIL;
+    const userEmail = user.emailAddresses[0]?.emailAddress
+
+    // if user is not admin
+if(!adminEmail || userEmail !== adminEmail) redirect("/dashboard")
+
+    return <div>
+        <AdminDashboardClient/>
+    </div>
+}
+export default AdminPage
